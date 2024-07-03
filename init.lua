31283912a6429ed718544e8aebb67bb4dbb7fb1d
Mathewos Endeshaw <contact.mathewos@gmail.com>
2024-07-03 14:08:54 +0300

    added wakatime and reverted back to gruvbox

--- init.lua
+++ init.lua
@@ -785,13 +785,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -921,6 +921,12 @@ if vim.g.neovide then
   vim.g.neovide_scale_factor = 0.75
   vim.g.neovide_cursor_animation_length = 0
 end
+
+-- netrw config
+-- taken from the primeagens config
+vim.g.netrw_browse_split = 0 -- netwr when opening a file will reuse the window (default)
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 75 -- the size of a new window opened with netrw using vertical (v) or horizontal (o) split in pecentage
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
